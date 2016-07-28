0cf4aa160931a7e5709add51c0c6757000d76169
Vardominator <barsegyanvarderes@gmail.com>
2016-07-27 18:24:26 -0700

    ASP.NET intro

---
+++ PersonalWebsite/PluralSight/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string something = Console.ReadLine();
+
+            Console.WriteLine($"Hello {something}!");
+            Console.WriteLine("We all hate apple products.");
+        }
+    }
+}
---
+++ PersonalWebsite/PluralSight/TheWorld/src/TheWorld/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TheWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Method that is called to set up what to do when requests come in. Code that is being written and called everytime a request comes in.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // As the request comes in, return "Hello World" as a response 
+            app.Run(async (context) =>
+            {
+                // Returns the string, not an actual webpage regardless of the request
+                await context.Response.WriteAsync("<html><body><h3>Hello World!<h3><body><html>");
+            });
+        }
+    }
+}
