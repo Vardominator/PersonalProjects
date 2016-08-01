c0a86532ae01db996f033610aa0582b200aef4b7
Vardominator <barsegyanvarderes@gmail.com>
2016-08-01 16:09:34 -0700

    holy fuck

--- PersonalWebsite/PluralSight/TheWorld/src/TheWorld/Controllers/Web/AppController.cs
+++ PersonalWebsite/PluralSight/TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -1,23 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _config = config;
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+
+        // Get based on route app/contact
         public IActionResult Contact()
         {
             return View();
+        }
+
+
+        // If someone posts to the action that is contact, this is the method that should be called
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support AOL addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+
         }
 
         public IActionResult About()
