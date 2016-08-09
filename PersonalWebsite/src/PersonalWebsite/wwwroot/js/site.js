b923115e9bf37d294b3a507ee0d01cd477511231
Vardominator <barsegyanvarderes@gmail.com>
2016-08-09 10:19:22 -0700

    added toggle sidebar

--- PersonalWebsite/src/PersonalWebsite/wwwroot/js/site.js
+++ PersonalWebsite/src/PersonalWebsite/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 ﻿// Write your Javascript code.
+(function () {
+
+
+    $("#sidebarToggle").click(function () {
+        $("#sidebar, #wrapper").toggleClass("hide-sidebar");
+        if ($("#sidebar").hasClass("hide-sidebar")) {
+            $(this).text("Show");
+        }
+        else {
+            $(this).text("Hide");
+        }
+    });
+
+    //$("sidebarToggle").on("click", function () {
+    //    $("#sidebar, #wrapper").toggleClass("hide-sidebar");
+    //});
+
+})();
